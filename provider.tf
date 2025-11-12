67d0186c315b90a428b7564e298f96cb99b1bc6e
Umaru0010 <5procentlife@gmail.com>
2025-11-13 00:33:45 +0500

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,7 @@
+provider "proxmox" {
+  pm_api_url      = "https://${var.proxmox_host}:8006/api2/json"
+  pm_user         = var.proxmox_user
+  pm_password     = var.proxmox_password
+  pm_tls_insecure = true
+  pm_timeout      = 600
+}
